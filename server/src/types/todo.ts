0ece7f52e198f57e29f58b598b23f91b894d7b66
ramzi hannachi <rhannachi1991@gmail.com>
2023-10-23 15:28:13 +0200

    Chore - clean code

--- server/src/types/todo.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose"
-
-// TODO interface that extends the Document type provided by mongoose. https://mongoosejs.com/docs/documents.html
-export interface ITodo extends Document {
-    name: string
-    description: string
-    status: boolean
-}
