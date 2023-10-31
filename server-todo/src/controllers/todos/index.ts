59706e405e062b9a01863e286113b669a2c78333
ramzi hannachi <rhannachi1991@gmail.com>
2023-10-31 17:35:20 +0100

    Refacto - Todo - add handler error

--- server-todo/src/controllers/todos/index.ts
+++ server-todo/src/controllers/todos/index.ts
@@ -2,30 +2,65 @@ import { Response, Request } from "express"
 import { ITodo } from "../../types/todo"
 import Todo from "../../models/todo"
 
+class ErrorApi {
+    message: string
+    status: number
+
+    constructor(status: number, message: string) {
+        this.message = message
+        this.status = status
+    }
+}
+
+const handlerErrorApi = (error: unknown | ErrorApi): [number, string] => {
+    let status = 500
+    let message = 'internal server error'
+
+    if (error instanceof ErrorApi) {
+        status = error.status
+        message = error.message
+    }
+
+    return [status, message]
+}
+
 export const getTodos = async (req: Request, res: Response): Promise<void> => {
     try {
         const todos: ITodo[] = await Todo.find()
         res.status(200).json({ todos })
     } catch (error) {
-        throw error
+        const [status, message] = handlerErrorApi(error)
+        res.status(status).json({ message });
     }
 }
 
 export const addTodo = async (req: Request, res: Response): Promise<void> => {
     try {
         const body = req.body as Pick<ITodo, "name" | "userId">
-      const todo: ITodo = new Todo({
-        name: body.name,
-        userId: body.userId,
-      })
-  
-      const newTodo: ITodo = await todo.save()
 
-      res
-        .status(200)
-        .json({ todo: newTodo })
+        if (!body.name) {
+            throw new ErrorApi(400, 'The name field does not exist')
+        }
+        if (!body.userId) {
+            throw new ErrorApi(400, 'The userId field does not exist')
+        }
+
+        const todo: ITodo = new Todo({
+            name: body.name,
+            userId: body.userId,
+        })
+
+        const newTodo = await todo.save()
+        if (!newTodo) {
+            throw new ErrorApi(404, 'Todo save failed')
+        }
+
+        res
+            .status(200)
+            .json({ todo: newTodo })
     } catch (error) {
-      throw error
+        const [status, message] = handlerErrorApi(error)
+        res.status(status).json({ message });
     }
   }
 
@@ -33,11 +68,15 @@ export const deleteTodo = async (req: Request, res: Response): Promise<void> => 
     try {
         const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
             req.params.id
-        )
+        ).catch(() => {
+            throw new ErrorApi(404, 'Todo remove failed')
+        })
+
         res.status(200).json({
             _id: deletedTodo?._id,
         })
     } catch (error) {
-        throw error
+        const [status, message] = handlerErrorApi(error)
+        res.status(status).json({ message });
     }
 }
